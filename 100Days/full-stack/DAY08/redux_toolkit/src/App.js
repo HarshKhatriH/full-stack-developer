1927b61cacc7aac5cadbb7402112658965484806
harsh khatri <harshkhatrih@gmail.com>
2025-07-27 16:27:08 +0530

    Day 09 Updating functionallity in redux toolkit(on day 08) file

--- 100Days/full-stack/DAY08/redux_toolkit/src/App.js
+++ 100Days/full-stack/DAY08/redux_toolkit/src/App.js
@@ -1,13 +1,15 @@
 // import { useState } from "react"
 import "./App.css"
 import { useDispatch, useSelector } from "react-redux"
-import { increment,decrement } from "./component/Redux/Store/Counterslice";
+import { increment,decrement, resetcount, actionpayload } from "./component/Redux/Store/Counterslice";
+import { useState } from "react";
 
 export default function App()
 {
 
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  const [amount,setAmount] = useState(0);
 
   function handleIncrement()
   {
@@ -18,6 +20,16 @@ export default function App()
   {
     dispatch(decrement())
   }
+
+  function resetHandler()
+  {
+    dispatch (resetcount())
+  }
+
+  function addHandler()
+  {
+    dispatch(actionpayload(amount))
+  }
   return(
     <div >
        <div style={{ backgroundColor:"skyblue", textAlign:"center" ,  border:"1px solid black" , height:"150px" , width:"400px", margin:"auto", marginTop:"20%"}}>
@@ -26,6 +38,14 @@ export default function App()
          <div>
           <button onClick={handleIncrement}>Increment</button>
           <button onClick={handleDecrement}>Decrement</button>
+          <button onClick={resetHandler}>Reset</button>
+         </div>
+
+         <div>
+          <input type="Number" placeholder="Enter value" value={amount} onChange={(e)=>{
+             setAmount(e.target.value)
+          }}></input>
+          <button onClick={addHandler}>Add</button>
          </div>
         
        </div>
--- 100Days/full-stack/DAY08/redux_toolkit/src/component/Redux/Store/Counterslice.jsx
+++ 100Days/full-stack/DAY08/redux_toolkit/src/component/Redux/Store/Counterslice.jsx
@@ -16,10 +16,19 @@ export const Counterslice = createSlice({
 
       decrement: (state) => {
         state.value -= 1
+      },
+
+      resetcount : (state) => {
+        state.value = 0;
+      },
+
+      actionpayload: (state,action) => {
+        state.value += Number(action.payload)
       }
+
 
   }
 })
 
-export const {increment,decrement} = Counterslice.actions;
+export const {increment,decrement,resetcount,actionpayload} = Counterslice.actions;
 export default Counterslice.reducer;
